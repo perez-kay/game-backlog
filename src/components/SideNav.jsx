e0d705e16d9769d76edb1f781ec038cd004b8bc3
Kayla Perez <kayla.n.perez404@gmail.com>
2025-01-22 12:03:59 -0800

    basic layout

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,23 @@
 import { NavLink } from 'react-router-dom';
 
 function SideNav() {
-  return <div>Side NavBar</div>;
+  return (
+    <nav className="flex flex-col items-center text-center bg-zinc-600">
+      <img src="/images/logo.png" alt="backlog logo" className="size-16" />
+      <NavLink to="/" className={`[&.active]:text-violet-400`}>
+        Homepage
+      </NavLink>
+      <NavLink to="/library" className={`[&.active]:text-violet-400`}>
+        Library
+      </NavLink>
+      {/* <NavLink to="/details" className={`[&.active]:text-violet-400`}>
+        Game Details
+      </NavLink> */}
+      <NavLink to="/search" className={`[&.active]:text-violet-400`}>
+        Search
+      </NavLink>
+    </nav>
+  );
 }
 
 export default SideNav;
